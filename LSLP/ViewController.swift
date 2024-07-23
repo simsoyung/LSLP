8a8f236144925d110a266b85d7dd734c026f9457
soyung sim <simsy1024@gmail.com>
2024-07-23 15:38:26 +0900

    Initial Commit

---
+++ LSLP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSLP
+//
+//  Created by 심소영 on 7/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
